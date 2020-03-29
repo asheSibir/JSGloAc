b7140159b25f9a818c8ec6da860954bdde27bbc6
asheSibir <56887042+asheSibir@users.noreply.github.com>
2020-03-29 21:53:21 +0300

    first commit

---
+++ Lesson 01/script01.js
@@ -0,0 +1,10 @@
+let money = 0,
+    income = 1,
+    addExpenses,
+    deposit,
+    mission, 
+    period;
+ 
+alert('4) Вывести на экран в модальном окне (alert) сообщение с любым текстом');
+
+console.log('5) Вывести в консоль сообщение с любым текстом');   
